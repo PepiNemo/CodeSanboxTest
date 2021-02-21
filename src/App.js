65d8fb8b3914d2de4efdb766978bd4337d205ae8
PepiNemo <67931071+PepiNemo@users.noreply.github.com>
2021-02-20 21:20:29 -0300

    formulario con 1 input y 1 icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./styles.css";
+import "bootswatch/dist/superhero/bootstrap.min.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Te amu sebita :3</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <form className="card card-body">
+        <div className="form-group input-group">
+          <div className="input-group-text bg-light">
+            <i className="material-icons">add</i>
+          </div>
+          <input
+            className="form-control"
+            name="url"
+            placeholder="www.website.com"
+            type="text"
+          />
+        </div>
+      </form>
     </div>
   );
 }
